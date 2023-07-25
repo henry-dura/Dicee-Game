b1ee462080f93be713377739f72f823368c3aa19
henry-dura <henryikemefuna@gmail.com>
2023-07-25 08:06:49 +0100

    add dice and fuctionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(MaterialApp(
-    home: SafeArea(
-      child: Scaffold(
-        body: Dicee(),
-        appBar: AppBar(
-          title: Text('Dicee Game'),
-          backgroundColor: Colors.purpleAccent,
+  runApp(DiceeStructure());
+}
+
+
+class DiceeStructure extends StatelessWidget {
+  const DiceeStructure({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white70,
+          body: Dicee(),
+          appBar: AppBar(
+            title: Center(child: Text('Dicee Game')),
+            backgroundColor: Colors.purpleAccent,
+          ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
+
 
 class Dicee extends StatefulWidget {
   const Dicee({Key? key}) : super(key: key);
@@ -22,13 +35,44 @@ class Dicee extends StatefulWidget {
 }
 
 class _DiceeState extends State<Dicee> {
+  int leftDiceNumber = 2;
+  int rightDiceNumber = 3;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Image.asset('images/dice1.png'),
-        Image.asset('images/dice2.png')
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset(
+                'images/dice$leftDiceNumber.png',
+                color: Colors.red,
+              ),
+            ),
+          ),
+          const SizedBox(width: 20),
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset(
+                'images/dice$rightDiceNumber.png',
+                color: Colors.red,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
